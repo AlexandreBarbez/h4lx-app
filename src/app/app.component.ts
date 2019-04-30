7f6df0889dd0de5cbc6e8c1637ce8f2de1c09d50
Alex <alexandre.barbez@gmail.com>
2019-04-30 18:05:09 +0200

    Ajout de la HP + menu hamburger.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'h4lx-app';
+  isExpanded: boolean;
+
+  signOut() {
+    console.log("signout clicked");
+  }
 }
